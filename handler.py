ae467ecd528694f26226fdb70d31b7e5d550b94f
Karolina Mikiciuk <karolina-o-mikiciuk@wp.pl>
2021-09-06 11:15:40 +0100

    fixing access issue with serverless and adding sqlalchemy to the dependencies

---
+++ handler.py
@@ -0,0 +1,18 @@
+
+import boto3
+import pandas as pa
+import io
+
+def handle(event):
+    # Get key and bucket informaition
+    key = event['Records'][0]['s3']['object']['key']
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    
+    # use boto3 library to get object from S3
+    s3 = boto3.client('s3')
+    s3_object = s3.get_object(Bucket = bucket, Key = key)
+    data = s3_object['Body'].read().decode('utf-8')
+    
+    # read CSV
+    df = pa.read_csv_file(io.StringIO(data))   
+    print(df)
